c8038f22d095f030ca699cd766652c99442b9730
MiguelCC82 <96418123+MiguelCCid@users.noreply.github.com>
2022-05-09 11:10:48 +0200

    Add files via upload

---
+++ Github/Examen final/script.js
@@ -0,0 +1,6 @@
+alert("Bienvenido al examen final")
+var nombre = prompt ("Dime tu nombre")
+alert("Hola " + nombre)
+if (nombre[0] == "a" || nombre[0] == "e" || nombre[0] == "i" || nombre[0] == "o" || nombre[0] == "u") {
+document.write ("La primera letra de tu nombre es " + nombre[0] + " y es una vocal" ); }
+else {document.write ("La primera letra de tu nombre es " + nombre[0] + " y es una consonante" ); } 
